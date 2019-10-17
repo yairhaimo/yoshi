3e64c4bc1a34a1029877ea838213437f42d0f13f
Amit Dahan <amitda@wix.com>
2019-10-17 12:39:00 +0300

    Add new (WIP) BM template

--- packages/create-yoshi-app/templates.js
+++ packages/create-yoshi-app/templates.js
@@ -10,6 +10,10 @@ module.exports = [
     name: 'business-manager-module',
     path: toTemplatePath('business-manager-module'),
   },
+  {
+    name: 'business-manager',
+    path: toTemplatePath('business-manager'),
+  },
   { name: 'server', path: toTemplatePath('server') },
   { name: 'library', path: toTemplatePath('library') },
   {
